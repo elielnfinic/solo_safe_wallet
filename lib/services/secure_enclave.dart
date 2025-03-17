33a084988f4bb90285901e9ca1a02f4f2024dcdc
Baruka David <baruka99.github@gmail.com>
2025-03-18 01:45:30 +0300

    refactoring the solo safe secure page

--- lib/services/secure_enclave.dart
+++ lib/services/secure_enclave.dart
@@ -1,12 +1,12 @@
-import 'package:secure_enclave/secure_enclave.dart';
+// import 'package:secure_enclave/secure_enclave.dart';
 
-Future<bool> initEnclave(String tag, String password) async {
-  final secureEnclave = SecureEnclave();
-  ResultModel res = await secureEnclave.generateKeyPair(
-      accessControl: AccessControlModel(tag: tag, password: password, options: [
-    AccessControlOption.applicationPassword,
-    AccessControlOption.privateKeyUsage
-  ]));
+// Future<bool> initEnclave(String tag, String password) async {
+//   final secureEnclave = SecureEnclave();
+//   ResultModel res = await secureEnclave.generateKeyPair(
+//       accessControl: AccessControlModel(tag: tag, password: password, options: [
+//     AccessControlOption.applicationPassword,
+//     AccessControlOption.privateKeyUsage
+//   ]));
 
-  return true;
-}
+//   return true;
+// }
