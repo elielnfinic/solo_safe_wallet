a1ae21f863d11a3ae15f7fa467293f956c3246d4
Baruka David <baruka99.github@gmail.com>
2025-03-18 00:17:10 +0300

    refactoring the app entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:solosafe/routes/app_routes.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:solosafe/start/check_private_key.dart';
+import 'package:solosafe/start/solosafe.dart';
 
 Future main() async {
   await dotenv.load(fileName: '.env');
-  runApp(MyApp());
-}
-
-class SoloSafe extends StatelessWidget {
-  const SoloSafe({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'SoloSafe Wallet',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: CheckPrivateKeyExists(),
-        routes: AppRoutes.routes);
-  }
+  runApp(SoloSafe());
 }
