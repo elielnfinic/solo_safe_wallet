b4bd84fa6e8d1dd98fda47f1c60ea297f130ea14
elielnfinic <46634774+elielnfinic@users.noreply.github.com>
2025-03-18 11:21:42 +0300

    test : Dummy test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,22 +10,10 @@
 
 // import 'package:solosafe/start/solosafe.dart';
 
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
-  // Removing this test as it is not needed
-  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(SoloSafe());
-
-  //   // Verify that our counter starts at 0.
-  //   // expect(find.text('0'), findsOneWidget);
-  //   // expect(find.text('1'), findsNothing);
-
-  //   // Tap the '+' icon and trigger a frame.
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-
-  //   // Verify that our counter has incremented.
-  //   // expect(find.text('0'), findsNothing);
-  //   // expect(find.text('1'), findsOneWidget);
-  // });
+  testWidgets('Dummy test for actions', (WidgetTester tester)async{
+    expect(1, 1);
+  });
 }
