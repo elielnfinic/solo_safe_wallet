ad712b78685b40d5127fd2407d398df02e8209fc
elielnfinic <46634774+elielnfinic@users.noreply.github.com>
2025-03-18 12:54:28 +0300

    fix : .env file path

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,14 +9,20 @@ import device_info_plus
 import flutter_blue_plus_darwin
 import network_info_plus
 import package_info_plus
+import path_provider_foundation
 import secure_enclave
+import secure_store
 import shared_preferences_foundation
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SecureEnclavePlugin.register(with: registry.registrar(forPlugin: "SecureEnclavePlugin"))
+  SecureStorePlugin.register(with: registry.registrar(forPlugin: "SecureStorePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
