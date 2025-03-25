ced8914398fcae810cd844250806687be0aa283c
elielnfinic <46634774+elielnfinic@users.noreply.github.com>
2025-03-25 15:20:35 +0300

    Tx

---
+++ lib/services/transaction.dart
@@ -0,0 +1,10 @@
+/// This file contains the logic for handling transactions.
+/// This includes creating transactions, getting transactions, and updating transactions.
+/// This file also contains the logic for handling the transaction history.
+
+
+Future<bool> mintAsset({required String amount, required String to}) async {
+  final walletKit = WalletKit();
+  final result = await walletKit.mintAsset(amount: amount, to: to);
+  return result;
+}
